5e797462a3b28a99d3f09fe5726f3cca7b4c1745
HAFIZ MUHAMMAD MOHSIN <78217218+Muhammad-Mohsin05@users.noreply.github.com>
2026-08-21 12:51:53 +0500

    Create RegisterPage.js

---
+++ RegisterPage.js
@@ -0,0 +1,11 @@
+fetch("/api/auth/register", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        username,
+        email,
+        password
+    })
+});
